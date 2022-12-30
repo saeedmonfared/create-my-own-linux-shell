06ffe18210c33fcf9b9668e8ea411a189f42e61c
saeedsmm <45818398+saeedmonfared@users.noreply.github.com>
2022-12-30 18:36:16 +0330

    finall changes
    add history file
    add hotkey for searching history lines
    add some special commands

--- myShell.c
+++ myShell.c
@@ -5,7 +5,8 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-
+#include <readline/readline.h>
+#include <readline/history.h>
 
 //global variables
 char current_directory[1000];
@@ -19,33 +20,42 @@ pid_t pid;
 int history_num = 0;
 
 //define
-#define BUFFER_SIZE 1000;
+#define BUFFER_SIZE 1000
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[1;31m"
+#define KGRN "\x1B[1;32m"
+#define KBLU "\x1B[1;34m"
 
 //functions header
 void prompt();
 void clearVar();
 void whit_pipe_execute();
-static int split(char *cmd_exec, int input, int first, int last);
+static int with_space_execute(char *cmd_exec, int input, int first, int last);
 void change_directory();
 void sigintHandler(int sig_num);
+void takeInput(char* str);
+void history_file();
 //...........
 
 int main(){
     sleep(1);
     char ch[2] = "\n";
     getcwd(current_directory, sizeof(current_directory));
-    //signal(SIGINT, sigintHandler);
+    signal(SIGINT, sigintHandler);
     system("clear");
     while(true){
         clearVar();
         prompt();
-        fgets(input_buffer, 1024, stdin);
+        //fgets(input_buffer, 1024, stdin);
+        takeInput(input_buffer);
+        history_file();
         if(strcmp(input_buffer, ch)==0)
         {
             continue;
         }
         len = strlen(input_buffer);
-        input_buffer[len-1] = '\0';
+        //input_buffer[len-1] = '\0';
         if(strcmp(input_buffer, "exit") == 0)
         {
             flag = 1;
@@ -54,13 +64,30 @@ int main(){
         whit_pipe_execute();
     }
     if(flag == 1){
-        printf("Bye...\n");
+        printf("%sBye...%s\n",KGRN, KNRM);
         sleep(1);
         system("clear");
         //exit(0);
         return 0;
     }
     return 0;
+}
+//echo hello >> h.txt
+
+void history_file(){
+    char his[1000];
+    strcpy(his, "echo ");
+    strcat(his, input_buffer);
+    strcat(his, ">> h.txt");
+    system(his);
+}
+void takeInput(char* str){
+    char* buf;
+    buf = readline("\n----->");
+    if (strlen(buf) != 0) {
+		add_history(buf);
+		strcpy(str, buf);
+	}
 }
 
 void sigintHandler(int sig_num){
@@ -77,15 +104,15 @@ void whit_pipe_execute(){
     cmd_exec[n] = NULL;
     pipe_count = n-1;
     for(i = 0; i < n-1; i++){
-        input = split(cmd_exec[i], input, first, 0);
+        input = with_space_execute(cmd_exec[i], input, first, 0);
         first=0;
     }
-    input = split(cmd_exec[i], input, first, 1);
+    input = with_space_execute(cmd_exec[i], input, first, 1);
     input=0;
     return;
 }
 
-static int split(char *cmd_exec, int input, int first, int last){
+static int with_space_execute(char *cmd_exec, int input, int first, int last){
     char *new_exec;
     new_exec = strdup(cmd_exec);
     int status;
@@ -100,27 +127,56 @@ static int split(char *cmd_exec, int input, int first, int last){
             return 1;
         }
         else if((pid = fork()) == 0){
-            //bakhsh marboot be execvp
-            status = execvp(args[0], args);
-            if(status == -1){
-                fprintf(stderr,"%s: command not found\n", args[0]);
-                exit(1);
+            if(strcmp(args[0], "a") == 0){
+                char buf[100] = "cut -d ' ' -f1 ";
+                strcat(buf,args[1]);
+                system(buf);
+                //printf("\n");
+                //exit(1);
             }
-        }
-        wait(NULL);
-        if(status != -1){
-            FILE *history_file = fopen("/home/saeed/Desktop/history", "w");
-            if(history_file == NULL) {
-                fprintf(stderr, "error: could not open the file\n");
-                return 1;
+            else if(strcmp(args[0], "b") == 0){
+                char buf[100] = "tr -cs A-Za-z '\n' < ";
+                strcat(buf, args[1]);
+                strcat(buf, " | tr A-Z a-z | sort | uniq -c | sort -rn | head -1 | tr -d ' ' | tr -d '[:digit:]'");
+                system(buf);
+                //exit(1);
+            }
+            else if(strcmp(args[0], "c") == 0){
+                char buf[100] = "a=$(tr -d \"[:space:]\" < ";
+                strcat(buf, args[1]);
+                strcat(buf, ")\n echo $a > ");
+                strcat(buf, args[1]);
+                strcat(buf, "\n cat ");
+                strcat(buf, args[1]);
+                system(buf);
+                //exit(1);
+            }
+            else if(strcmp(args[0], "d") == 0){
+                char buf[100] = "grep -v '^#' ";
+                strcat(buf, args[1]);
+                system(buf);
+            }
+            else if(strcmp(args[0], "f") == 0){
+                char buf[100] = "wc -l < ";
+                strcat(buf, args[1]);
+                system(buf);
+            }
+            else if(strcmp(args[0], "g") == 0){
+                char buf[100] = "head -10 ";
+                strcat(buf, args[1]);
+                system(buf);
             }
             else{
-                history_num++;
-                fprintf(history_file,"%d- %s\n", history_num, cmd_exec);
+                //bakhsh marboot be execvp
+                status = execvp(args[0], args);
+                if(status == -1){
+                    fprintf(stderr,"%s: %scommand not found%s\n", args[0], KRED, KNRM);
+                    //exit(1);
+                }
             }
-            fclose(history_file);
+            exit(1);
         }
-
+        wait(NULL);
     }
     return 1;
 }
@@ -137,7 +193,7 @@ void change_directory(){
             system("clear");
         }
         else{
-            printf("bash: cd: %s: No such file or directory\n", args[1]);
+            printf("bash: cd: %s: %sNo such file or directory%s\n", args[1], KRED, KNRM);
         }
     }
 
@@ -147,9 +203,10 @@ void prompt(){
     char shell[1000];
     if(getcwd(pwd, sizeof(pwd)) != NULL){
         strcpy(shell , "saeed_and_behnam shell:");
+        //printf("%ssaeed@behnam_shell:%s",KGRN,KNRM);
         strcat(shell, pwd);
-        strcat(shell, "$ ");
-        printf("%s", shell);
+        //strcat(shell, "$ ");
+        printf("%s%s%s$", KBLU, shell, KNRM);
     }
     else{
         perror("getcwd() error");
